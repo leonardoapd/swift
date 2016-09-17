07a2620a90c2e8203aa8cd30a05ecd9bc95eba2a
Leonardo <leonardop763@gmail.com>
2016-09-17 16:56:27 -0500

    Juego de Memoria
    Tarea de la Semana 2

--- semana2.playground/Contents.swift
+++ semana2.playground/Contents.swift
@@ -8,9 +8,11 @@ for x in rango {
     
     if x >= 30 && x <= 40  {
         print("\(x) Viva Swift")
-    }else  if x % 5 == 0{
+    }
+    if x % 5 == 0{
         print("\(x) Bingo!!!")
-    }else if x % 2 == 0 {
+    }
+    if x % 2 == 0 {
         print("\(x) El numero es par")
     }else if x % 2 == 1 {
         print("\(x) El numero es impar")
