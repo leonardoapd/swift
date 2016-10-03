820960eeac7fcd79382d58b881b55ef8877b82e3
Leonardo <leonardop763@gmail.com>
2016-10-02 20:45:07 -0500

    Semana 4 - Velocimetro
    Velocimetro Digital

--- semana4.playground/Contents.swift
+++ semana4.playground/Contents.swift
@@ -6,32 +6,65 @@ enum Velocidades : Int {
     case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
     
     init(velocidaInicial : Velocidades) {
-        self = .Apagado
+        self = velocidaInicial
     }
 }
 
-enum VelocidadesEnCadena : String {
-    case  Apagado = "Apagado", VelocidadBaja = "Velocidad baja", VelocidadMedia = "Velocidad Media", VelocidadAlta = "Velocidad Alta"
-    
-    init(velocidaInicial : VelocidadesEnCadena) {
-        self = .Apagado
-    }
-}
+
 
 class Auto {
-    var velocidad : Velocidades
-    var velocidadesEnCadena : VelocidadesEnCadena
     
-    init(velocidad : Velocidades, velocidadesEnCadena : VelocidadesEnCadena) {
-        self.velocidad = velocidad
-        self.velocidadesEnCadena = velocidadesEnCadena
+    var velocidad : Velocidades
+    var velocidadActual : Int = -1
+    var msj : String
+    
+    init(velocidad : Velocidades, msj : String) {
+        self.velocidad = Velocidades(velocidaInicial: Velocidades.Apagado)
+        self.msj = ""
+        
     }
     
-    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String) {
-            return (velocidad.rawValue, velocidadesEnCadena.rawValue)
+    func cambioDeVelocidad(actual : Int, velocidadEnCadena : String) -> (Int , String){
+        
+        self.velocidadActual = actual
+        var resultado = (actual,velocidadEnCadena)
+        
+        if  self.velocidadActual == -1 {
+            self.velocidadActual = Velocidades.Apagado.rawValue
+            msj = "Apagado"
+            resultado = (self.velocidadActual,msj)
+        }
+        else if  self.velocidadActual == 0 {
+            self.velocidadActual = Velocidades.VelocidadBaja.rawValue
+            msj = "Velocidad baja"
+            resultado = (self.velocidadActual,msj)
+        }
+        else if self.velocidadActual == 20 {
+            self.velocidadActual = Velocidades.VelocidadMedia.rawValue
+            msj = "Velocidad Media"
+            resultado = (self.velocidadActual,msj)
+        }
+        else if self.velocidadActual == 50 {
+            self.velocidadActual = Velocidades.VelocidadAlta.rawValue
+            msj = "Velocidad Alta"
+            resultado = (self.velocidadActual,msj)
+        }
+        else if self.velocidadActual == 120 {
+            self.velocidadActual = Velocidades.VelocidadMedia.rawValue
+            msj = "Velocidad Media"
+            resultado = (self.velocidadActual,msj)
+        }
+        return resultado
+        
     }
 }
 
 
+var auto = Auto(velocidad: Velocidades.VelocidadBaja, msj: "Encendido")
 
+for i in 0 ..< 20 {
+    
+    print(auto.cambioDeVelocidad(actual: auto.velocidadActual, velocidadEnCadena: "Encendido"))
+    
+}
 
